12f1b3ddd3f8b2c00658fe1b3adef7669983cb74
Rawrtor <hectoryjorge2021@gmail.com>
2025-08-27 23:02:01 -0600

    V0.1.1

--- h2optimizer/lib/pages/_StatisticsContent.dart
+++ h2optimizer/lib/pages/_StatisticsContent.dart
@@ -15,69 +15,71 @@ class _StatisticsContentState extends State<StatisticsContent> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          SizedBox(
-            // Widget para fijar el tamaño
-            width: 300, // Ancho deseado
-            height: 150, // Alto deseado
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                      builder: (context) => const ETongueScreen()),
-                );
-              },
-              child: const Text('E-tounge'),
+      child:SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              // Widget para fijar el tamaño
+              width: 300, // Ancho deseado
+              height: 150, // Alto deseado
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                        builder: (context) => const ETongueScreen()),
+                  );
+                },
+                child: const Text('E-tounge'),
+              ),
             ),
-          ),
-          const SizedBox(height: 16),
-          SizedBox(
-            // Widget para fijar el tamaño
-            width: 300,
-            height: 150,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                      builder: (context) => const TemperatureScreen()),
-                );
-              },
-              child: const Text('Temperatura'),
+            const SizedBox(height: 16),
+            SizedBox(
+              // Widget para fijar el tamaño
+              width: 300,
+              height: 150,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                        builder: (context) => const TemperatureScreen()),
+                  );
+                },
+                child: const Text('Temperatura'),
+              ),
             ),
-          ),
-          const SizedBox(height: 16),
-          SizedBox(
-            // Widget para fijar el tamaño
-            width: 300,
-            height: 150,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                      builder: (context) => const TurbidityScreen()),
-                );
-              },
-              child: const Text('Turbidez'),
+            const SizedBox(height: 16),
+            SizedBox(
+              // Widget para fijar el tamaño
+              width: 300,
+              height: 150,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                        builder: (context) => const TurbidityScreen()),
+                  );
+                },
+                child: const Text('Turbidez'),
+              ),
             ),
-          ),
-          const SizedBox(height: 16),
-          SizedBox(
-            // Widget para fijar el tamaño
-            width: 300,
-            height: 150,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                      builder: (context) => const ElectrochemicalScreen()),
-                );
-              },
-              child: const Text('Electroquímicos'),
+            const SizedBox(height: 16),
+            SizedBox(
+              // Widget para fijar el tamaño
+              width: 300,
+              height: 150,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                        builder: (context) => const ElectrochemicalScreen()),
+                  );
+                },
+                child: const Text('Electroquímicos'),
+              ),
             ),
-          ),
-        ],
+          ],
+        )
       ),
     );
   }
